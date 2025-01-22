e99d1380a828c21bc0669fd253d88743153de0ff
sponcevas <129618132+ZamudiaJr@users.noreply.github.com>
2025-01-22 11:52:07 -0400

    database for client complete

---
+++ app/clients/infraestructure/db/database.py
@@ -0,0 +1,25 @@
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from decouple import config
+
+DATABASE_URL = config('DATABASE_URL') 
+#"sqlite+aiosqlite:///./test.db"
+
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=True, 
+    future=True
+)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+    async with async_session() as session:
+        yield session
